498c28dc320ba0740ee8bb3857f420c629b1e08e
René Föhring <rf@bamaru.de>
2012-12-22 12:39:42 +0100

    Update summary task

--- lib/task/summary.rb
+++ lib/task/summary.rb
@@ -8,8 +8,7 @@ module Task
         filename = template.filename.gsub(build.__path__, '')
         puts filename.dark
         puts 
-        puts template.summary.gsub(/^\s*(\S)/, '    \1')
-        puts
+        puts spaces_at_start_of_each_line(template.summary)
         puts
       end
     end
@@ -20,6 +19,10 @@ module Task
       }.reverse.map { |subarray|
         subarray.sort { |x,y| x.filename <=> y.filename }
       }.flatten
+    end
+
+    def spaces_at_start_of_each_line(str, spaces = '    ')
+      str.gsub(/^\s*(\S)/, spaces + '\1')
     end
 
   end
