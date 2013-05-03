1c5a114ee36526e4ba2d7d1e9ee2e0b6adf6a376
René Föhring <rf@bamaru.de>
2013-05-03 17:55:13 +0200

    Fix broken Task::Watch

--- lib/texas/task/watch.rb
+++ lib/texas/task/watch.rb
@@ -15,25 +15,28 @@ module Texas
         end
       end
 
-      def self.directories_to_watch
-        arr = []
-        arr << "tex"      if Dir.exists?("tex")
-        arr << "contents" if Dir.exists?("contents")
-        arr << "figures"  if Dir.exists?("figures")
-        arr << Texas.texas_dir
-        arr
-      end
+      class << self
+        include Texas::OutputHelper
 
-      def self.rebuild
-        started_at = Time.now.to_i
-        Build::Final.new(run_options).run
-        finished_at = Time.now.to_i
-        trace (finished_at - started_at).to_s + " seconds to rebuild"
-      rescue Exception => e
-        trace @build.current_template.filename
-        trace "[ERROR] while building \n#{e}"
-      end
+        def directories_to_watch
+          arr = []
+          arr << "tex"      if Dir.exists?("tex")
+          arr << "contents" if Dir.exists?("contents")
+          arr << "figures"  if Dir.exists?("figures")
+          arr << Texas.texas_dir
+          arr
+        end
 
+        def rebuild
+          started_at = Time.now.to_i
+          Build::Final.new(run_options).run
+          finished_at = Time.now.to_i
+          trace (finished_at - started_at).to_s + " seconds to rebuild"
+        rescue Exception => e
+          trace @build.current_template.filename
+          trace "[ERROR] while building \n#{e}"
+        end
+      end
     end
   end
 end
