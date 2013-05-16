ee3348209e7a4b1d3783216b67ef065636ba78a8
René Föhring <rf@bamaru.de>
2013-05-16 11:14:03 +0200

    Fix 'stack level too deep' in TemplateError

--- lib/texas/template/template_error.rb
+++ lib/texas/template/template_error.rb
@@ -8,7 +8,7 @@ class TemplateError < StandardError
     parse_backtrace_for_origin
   end
 
-  def backtrace
+  def __backtrace__
     original ? original.backtrace : backtrace
   end
 
@@ -17,12 +17,12 @@ class TemplateError < StandardError
   end
 
   def parse_backtrace_for_origin
-    arr = backtrace
+    arr = __backtrace__ || []
     arr.each_with_index do |line, index| 
       if line =~ /\(erb\):(\d+)/ 
         @pre_erb_backtrace = arr[0...index]
         @line_number = $1.to_i
-        line_before = backtrace[index-1]
+        line_before = arr[index-1]
         @method_name = line_before =~ /\d+:in\s+`([^\)]+)'/ && $1
         return
       end
@@ -34,13 +34,15 @@ class TemplateError < StandardError
   end
 
   def pre_erb_backtrace
-    arr = @pre_erb_backtrace.map do |line| 
-      line.gsub(template.build.root, '.')
+    if @pre_erb_backtrace
+      arr = @pre_erb_backtrace.map do |line| 
+        line.gsub(template.build.root, '.')
+      end
+      str = arr.empty? ? "" : arr.join("\n").dark + "\n"
     end
-    str = arr.empty? ? "" : arr.join("\n").dark + "\n"
   end
 
   def message
-    super + "\n" + pre_erb_backtrace + origin.cyan
+    super + "\n#{pre_erb_backtrace}#{origin.cyan}"
   end
 end
